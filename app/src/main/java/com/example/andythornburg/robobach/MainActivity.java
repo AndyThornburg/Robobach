b48306c90b0e8c21226c72d359ed581c7190fd10
andythornburg <athornbu@unca.edu>
2016-03-23 22:43:17 -0400

    add ic_launcher.png

--- app/src/main/java/com/example/andythornburg/robobach/MainActivity.java
+++ app/src/main/java/com/example/andythornburg/robobach/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.andythornburg.robobach;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.R;
 import android.util.Log;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
