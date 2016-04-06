ba4980a8773a48dacad9cd519edabfe888755e8a
andythornburg <athornbu@unca.edu>
2016-04-06 19:25:40 -0400

    added setters

--- app/src/main/java/com/example/andythornburg/robobach/model/ListView.java
+++ app/src/main/java/com/example/andythornburg/robobach/model/ListView.java
@@ -22,15 +22,35 @@ public class ListView {
 
     public int getdiscnum() {return discnum;}
 
+    public void setdiscnum(int discnum) {
+        this.discnum = discnum;
+    }
+
     public String getavailablemarkets() {
         return available_markets;
     }
 
+    public void setavailablemarkets(String available_markets) {
+        this.available_markets = available_markets;
+    }
+
     public int getDurationms() { return duration_ms; }
+
+    public void setDurationms(int durationms) {
+        this.duration_ms = durationms;
+    }
 
     public boolean getExplicit() { return explicit; }
 
+    public void setExplicit(boolean explicit) {
+        this.explicit = explicit;
+    }
+
     public int getDiscNum() { return disc_number; }
+
+    public void setDiscNum(int discnum) {
+        this.discnum = discnum;
+    }
 
     public String getHref() {
         return href;
@@ -48,14 +68,28 @@ public class ListView {
         this.id = id;
     }
 
-    public boolean setPlayable() {return is_playable;}
+    public boolean getPlayable() {return is_playable;}
 
-    public String getPreview_url() {return preview_url;}
+    public void setPlayable(boolean is_playable) {
+        this.is_playable = is_playable;
+    }
+
+    public String getPreviewurl() {return preview_url;}
+
+    public void setPreviewurl(String preview_url) { this.preview_url = preview_url; }
 
     public int getTrackNumber() {return track_number;}
 
+    public void setTrackNumber(int track_number) {
+        this.track_number = track_number;
+    }
+
     public String getType() {return type;}
 
+    public void setType(String type) { this.type = type; }
+
     public String geturi() {return uri;}
+
+    public void seturi(String uri) { this.uri = uri; }
 
 }
